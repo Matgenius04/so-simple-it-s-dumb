75641504b04f0583efc08a744da48ecf6bd9fb11
drcrotato <31459944+drcrotato@users.noreply.github.com>
2019-03-10 22:50:44 -0400

    switching to canvas and starting to work on barrel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const port = 3000;
 const http = require('http');
 let clock = 50; //milleseconds between sending updates
-const express = require('express')
+const express = require('express');
 const app = express();
 const server = http.createServer(app)
 const io = require('socket.io').listen(server);
@@ -12,14 +12,24 @@ let list = {
     people:[]
 }
 io.on('connection', socket => {
+    let latency = ()=>{
+        socket.emit('latency',Date.now(),starttime=>{
+            return Date.now() - starttime;
+        });
+    };
+    let movementMessages = {up:0,left:0,down:0,right:0};
+    setInterval(()=>{
+        movementMessages = {up:0,left:0,down:0,right:0};
+    },clock)
     console.log('someone connected');
     socket.on('position', (position) => {
-    let color = ['red','blue','orange','yellow','black','green']
-    let pickColor = Math.floor(Math.random()*6)
+    let color = ['red','blue','orange','yellow','black','green'];
+    let pickColor = Math.floor(Math.random()*6);
         let person = {
             xPosition: position.x,
             yPosition: position.y,
-            color: color[pickColor]
+            color: color[pickColor],
+            barrelAngle: 0
         }
         list.ids.push(socket.id);
         list.people.push(person);
@@ -27,31 +37,47 @@ io.on('connection', socket => {
         socket.emit('numberid',list.ids.length - 1);
         socket.emit('update',list.people);
     })
+    socket.on('barrelAngle',angle=>{
+        const find = list.ids.findIndex(id => id == socket.id);
+        if (list.people[find]) {
+            list.people[find].barrelAngle = angle;
+        }
+    })
     socket.on('move', (direction) => {
         const find = list.ids.findIndex(id => id == socket.id);
         if (list.people[find]) {
             if(direction=='up') {
-            list.people[find].yPosition += 3;
+            movementMessages.up++;
+            list.people[find].yPosition += -2;
         }
         if(direction=='left') {
-            list.people[find].xPosition += -3;
+            movementMessages.left++;
+            list.people[find].xPosition += -2;
         }
         if(direction=='down') {
-            list.people[find].yPosition += -3;
+            movementMessages.down++;
+            list.people[find].yPosition += 2;
         }
         if(direction=='right') {
-            list.people[find].xPosition += 3;
+            movementMessages.right++;
+            list.people[find].xPosition += 2;
         }
         }
-    })
+        if (movementMessages == undefined) {
+            movementMessages = {up:0,left:0,down:0,right:0};
+        }
+        if (movementMessages.up>6 || movementMessages.left>6 || movementMessages.down>6 || movementMessages.right>6) {
+            socket.emit('hacker',movementMessages);socket.disconnect()
+        }
+    });
     socket.on('disconnect', ()=>{
-        console.log('user disconnected')
+        console.log('user disconnected');
         console.log(list)
         const find = list.ids.findIndex(id => id == socket.id);
         list.people.splice(list.people[find],1)
         list.ids.splice(list.ids[find],1)
         for(let x=0;x<list.people.length;x++){
-            sendNumberId(x)
+            sendNumberId(x);
         }
     })
 })
--- public/main.js
+++ public/main.js
@@ -1,62 +1,98 @@
 let socket = io();
 let lastpeople;
 let numberId;
-const canvas = document.getElementsById('canvas');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let mouseX;
+let mouseY;
+let screenX = window.innerWidth/2;
+let screenY = window.innerHeight/2;
+document.getElementsByTagName('body')[0].style.margin = "0 auto";
 socket.emit('position', {
   x: 0,
   y: 0
 });
 socket.on('numberid', number => {
   numberId = number;
-})
+  alert(numberId);
+});
+socket.on('hacker',(moveMentMessages)=>{
+  console.log(moveMentMessages);
+  alert('Stop trying to hack the game. This session has been terminated.');
+});
+// document.getElementById('canvas').style.width = `${window.innerWidth}px`;
+// document.getElementById('canvas').style.height = `${window.innerHeight}px`;
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+ctx.translate(screenX,screenY);
+ctx.scale(screenX*2/1080,screenX*2/1080);
 window.addEventListener('resize', () => {
-  let screenX = window.innerWidth/2;
-  let screenY = window.innerHeight/2;
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  // document.getElementById('canvas').style.width = `${window.innerWidth}px`;
+  // document.getElementById('canvas').style.height = `${window.innerHeight}px`;
+  screenX = window.innerWidth/2;
+  screenY = window.innerHeight/2;
   ctx.translate(screenX,screenY);
-})
+  ctx.scale(screenX*2/1080,screenX*2/1080);
+});
 let keys = [];
-setTimeout(()=>{
+window.addEventListener('load',()=>{
+  setTimeout(()=>{
+
 window.addEventListener('keydown', e => {
   keys[e.keyCode] = true;
-  console.log(e.keyCode);
 }, false);
 
 window.addEventListener('keyup', e => {
   keys[e.keyCode] = false;
-}, false)
-},50)
+}, false);
 
+window.addEventListener('mousemove',e=>{
+  screenX = window.innerWidth/2;
+  screenY = window.innerHeight/2;
+  mouseX = e.clientX-screenX-lastpeople[numberId].xPosition;
+  mouseY = e.clientY-screenY-lastpeople[numberId].yPosition;
+  // console.log(lastpeople[numberId].xPosition)
+  // console.log((Math.atan2(mouseY,mouseX)*180)/Math.PI);
+});
+},500);
+
+});
 
 setInterval(() => {
   if (keys[87]) {
-    console.log('keydown w')
+    // console.log('keydown w')
     socket.emit('move', 'up');
     //client prediction
     // lastpeople[numberId].yPosition += 3;
     // updatePos()
   }
   if (keys[65]) {
-    console.log('keydown a')
+    // console.log('keydown a')
     socket.emit('move', 'left');
     //client prediction
     // lastpeople[numberId].xPosition += -3;
     // updatePos()
   }
   if (keys[83]) {
-    console.log('keydown s')
+    // console.log('keydown s')
     socket.emit('move', 'down');
     //client prediction
     // lastpeople[numberId].yPosition += -3;
     // updatePos()
   }
   if (keys[68]) {
-    console.log('keydown d')
+    // console.log('keydown d')
     socket.emit('move', 'right');
     //client prediction
     // lastpeople[numberId].xPosition += 3;
     // updatePos()
   }
+  socket.emit('barrelAngle',Math.atan2(mouseY,mouseX));
+  socket.on('latency',(starttime,cb)=>{
+    cb(starttime);
+  });
 }, 50);
 
 //client prediction
@@ -81,10 +117,8 @@ let updatePos = () => {
 //server updates
 socket.on('update', people => {
   lastpeople = people;
-  for (let x=0; x<people.length;x++) {
-    window.requestAnimationFrame(draw)
-  }
-
+  ctx.clearRect(-canvas.width/2,-canvas.height/2,canvas.width,canvas.height);
+  window.requestAnimationFrame(draw);
   //html version
   // if (document.getElementsByClassName('player').length) {
   //   for (let r = 0; r < document.getElementsByClassName('player').length; r++) {
@@ -99,9 +133,26 @@ socket.on('update', people => {
   //   circle.setAttribute('class', 'player');
   //   document.getElementById('fakecanvas').appendChild(circle);
   // }
-})
+});
 
-let draw = () => {
+function draw() {
+  ctx.fillRect(-5,-5,10,10);
   let circle = new Path2D();
-  circle.arc()
+  let barrel = new Path2D();
+  for (let x=0; x<lastpeople.length;x++) {
+    ctx.save();
+    ctx.translate(lastpeople[x].xPosition,lastpeople[x].yPosition);
+    ctx.rotate(lastpeople[x].barrelAngle-(Math.PI/2));
+    barrel.rect(-3,-3,6,40);
+    ctx.fillStyle = 'gray';
+    ctx.fill(barrel);
+    ctx.restore();
+    ctx.fillStyle = lastpeople[x].color;
+    circle.arc(lastpeople[x].xPosition,lastpeople[x].yPosition,20,0,Math.PI*2,true);
+    ctx.fill(circle);
+  }
 }
+
+socket.on('disconnect',()=>{
+  console.log('DISCONNECTED');
+});
